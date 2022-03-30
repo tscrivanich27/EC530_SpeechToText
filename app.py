09a6cdf49e642b5e3a70b39a52026cef067b9466
tscrivanich27 <tscrivanich27@gmail.com>
2022-03-30 13:15:36 -0400

    Added Comments to Code

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
+# app.py Dependencies
 from celery import Celery 
 import speech_recognition as sr
 
+# Initialize the recognizer class and set to variable r
 r = sr.Recognizer()
 
+# Initialize a new celery app
+# Hosted on Redis server: localhost, port 6379
+# SQL Database: results.db
 app = Celery("app",
              broker="redis://localhost:6379",
              backend="db+sqlite:///results.db")
 
+# Define a new task for the queue system
 @app.task
+# Function speech_to_text converts an audio file to text
+# Parameter: name of audio file
 def speech_to_text(audio_file): 
+    # Open the audio file and set to variable source
     with sr.AudioFile(audio_file) as source:
+        # Listen to the audio file using r.listen
         audio_text = r.listen(source)
 
+        # Attempt to recognize the contents of the file
         try:
+            # Use Google speech recognition to convert audio to text
             text = r.recognize_google(audio_text)
+            # Return the generated message
             return ("Converting audio to text...\n" + text)
+        # If the contents of the file cannot be converted
         except:
+            # Return an error message
             return("Sorry...please try again.")
--- test.py
+++ test.py
@@ -1,29 +1,41 @@
+# test.py Dependencies
 from app import speech_to_text
 import logging
 
+# Initialize the log file, example.log
 logging.basicConfig(filename='example.log', filemode='w', level=logging.DEBUG)
 
 # Unit Test #1: Single Valid Audio File 
 def test_single_valid_audio():
     logging.info("Running Unit Test #1")
     try:
+        # Have celery run the speech to text task
         speech_to_text.delay("Test #1.wav")
+        # Use an assertion to check the result of speech_to_text function
         assert speech_to_text("Test #1.wav") == "Converting audio to text...\n" + "hello how are you doing today"
+        # Print success message
         logging.info("Unit Test #1 Succeeded")
+    # If the assertion returns false...
     except (AssertionError):
+        # Print error message
         logging.error("Unit Test #1 Failed")
 
 # Unit Test #2: Single Invalid Audio File 
 def test_single_invalid_audio():
     logging.info("Running Unit Test #2")
     try:
+        # Have celery run the speech to text task
         speech_to_text.delay("Test #2.wav")
+        # Use an assertion to check the result of speech_to_text function
         assert speech_to_text("Test #2.wav") == "Sorry...please try again."
+        # Print success message
         logging.info("Unit Test #2 Succeeded")
+    # If the assertion returns false...
     except (AssertionError):
+         # Print error message
         logging.error("Unit Test #2 Failed")
 
-# Main
+# Main Function
 if __name__ == "__main__":
     test_single_valid_audio()
     test_single_invalid_audio()
