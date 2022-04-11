8e8dcd64ecdec96b271b4392c0c96b22ebf7719b
tscrivanich27 <tscrivanich27@gmail.com>
2022-04-11 10:00:51 -0400

    Implemented Unit Test #3

--- test.py
+++ test.py
@@ -32,11 +32,33 @@ def test_single_invalid_audio():
         logging.info("Unit Test #2 Succeeded\nResults:\n" + speech_to_text("Test #2.wav"))
     # If the assertion returns false...
     except (AssertionError):
-         # Print error message
+        # Print error message
         logging.error("Unit Test #2 Failed")
+
+# Unit Test #3: Testing the queue system with four valid audio files 
+def test_queue_system():
+    logging.info("Running Unit Test #3")
+    try:
+        # Have celery run the speech to text task
+        speech_to_text.delay("Test #3.wav")
+        speech_to_text.delay("Test #4.wav")
+        speech_to_text.delay("Test #5.wav")
+        speech_to_text.delay("Test #6.wav")
+        # Set text message to variable result
+        result = speech_to_text("Test #3.wav") + " " + speech_to_text("Test #4.wav") +  " " + speech_to_text("Test #5.wav" ) + " " + speech_to_text("Test #6.wav")
+        # Use an assertion to check the result of speech_to_text function
+        assert result == "Converting audio to text...\nhello my name is Thomas Converting audio to text...\nthe temperature outside is 44 degrees Fahrenheit Converting audio to text...\nit is very sunny today it is a beautiful day Converting audio to text...\nI hope you are having a good day today"
+        # Print success message
+        logging.info("Unit Test #3 Succeeded\nResults:\n" + speech_to_text("Test #3.wav") + "\n" + speech_to_text("Test #4.wav") +
+            "\n" + speech_to_text("Test #5.wav") + "\n" + speech_to_text("Test #6.wav"))
+    # If the assertion returns false...
+    except (AssertionError):
+        # Print error message
+        logging.error("Unit Test #3 Failed")
 
 # Main Function
 if __name__ == "__main__":
     test_single_valid_audio()
     test_single_invalid_audio()
+    test_queue_system()
     logging.info("Ran All Tests")
